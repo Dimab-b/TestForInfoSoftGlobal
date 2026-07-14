ffd7d3cb2458cb052403d4d48e0c1e7575425eeb
dimas <you@example.comdimon3456lolkek@gmail.com>
2026-07-14 17:10:31 +0300

    done persistance with appdbcontext , seed data

--- InfosoftGlobalTestApi/Program.cs
+++ InfosoftGlobalTestApi/Program.cs
@@ -1,11 +1,24 @@
+using InfosoftGlobalTestApi.Domain.Common;
+using InfosoftGlobalTestApi.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=infosoft_test.db"));
 
+builder.Services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<AppDbContext>());
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+}
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
