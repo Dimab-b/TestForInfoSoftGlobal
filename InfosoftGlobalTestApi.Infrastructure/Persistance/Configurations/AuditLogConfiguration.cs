ea81d78543c903a95c522b3a2ec928e1e7b9953d
dimas <you@example.comdimon3456lolkek@gmail.com>
2026-07-14 16:52:02 +0300

    Added AuditLog Configuration to database

---
+++ InfosoftGlobalTestApi.Infrastructure/Persistance/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using InfosoftGlobalTestApi.Domain.AuditLogs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfosoftGlobalTestApi.Infrastructure.Persistance.Configurations
+{
+    public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
+    {
+        public void Configure(EntityTypeBuilder<AuditLog> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.EntityId)
+                   .IsRequired();
+
+            builder.HasIndex(a => a.EntityId);
+
+            builder.Property(a => a.Action)
+                   .IsRequired()
+                   .HasMaxLength(255);
+
+            builder.Property(a => a.CreatedAt)
+                   .IsRequired();
+        }
+    }
+}
